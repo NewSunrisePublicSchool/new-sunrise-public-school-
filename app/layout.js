a10a7fa60c7d7623da47b2a0ce4d6779df017a2b
NewSunrisePublicSchool <mdsadaquat370@gmail.com>
2026-09-01 15:15:56 +0530

    Build New Sunrise Public School website

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'New Sunrise Public School',description:'Quality education from Nursery to Class 8'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
